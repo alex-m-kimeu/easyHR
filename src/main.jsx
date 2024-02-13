7d0b151493bd43a478111b51d743bc8862e3b9b5
Alex Kimeu <alexkimeu1999@gmail.com>
2024-02-14 00:08:03 +0300

    Deployment bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './components/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/easyHR/">
+  <BrowserRouter basename="easyHR">
     <App/>
   </BrowserRouter>
 )
