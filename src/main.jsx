d740152fad0dc908e256cc282bde33100a685885
Alex Kimeu <alexkimeu1999@gmail.com>
2024-02-14 10:01:21 +0300

    Typo fix in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './components/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}>
+  <BrowserRouter basename={import.meta.env.DEV ? '/' : '/easyHR/'}>
     <App/>
   </BrowserRouter>
 )
